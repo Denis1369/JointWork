6b7aaae8fcd413b7f221d5789f88842c5b5efe9b
Adelya145 <badamshinaadelina19@gmail.com>
2025-05-22 12:37:30 +0500

    изменение новостей

--- User/Dent/Model/News.cs
+++ User/Dent/Model/News.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Dent.Model;
 
@@ -14,6 +15,9 @@ public partial class News
     public DateOnly? DatePublish { get; set; }
 
     public byte[]? NewsImage { get; set; }
+
+    [Column("news_status", TypeName = "enum('Ожидание','Публикация','Убрано')")]
+    public string? NewsStatus { get; set; }
 
     public byte[]? DisplayedImage => NewsImage;
 }
