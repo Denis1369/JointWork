41ae78d44cbc04dfe03ce93f60a4faaf168b0a71
Denis1369 <denisiippp@gmail.com>
2025-05-28 17:36:51 +0500

    Add User app

---
+++ User/Dent/Model/Service.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dent.Model;
+
+public partial class Service
+{
+    public int ServicesId { get; set; }
+
+    public string? ServicesTitle { get; set; }
+
+    public string? ServicesDesc { get; set; }
+
+    public int? ServicesPrice { get; set; }
+
+    public int? ServicesTypeId { get; set; }
+
+    public virtual ServicesType? ServicesType { get; set; }
+}
